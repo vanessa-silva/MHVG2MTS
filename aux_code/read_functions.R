3f377e72df3849f06f55802d9383a5ebc5bc4dc2
vanessa-silva <vanessaaf_silva@hotmail.com>
2022-09-13 11:24:56 +0100

    updating aux read functions

--- aux_code/read_functions.R
+++ aux_code/read_functions.R
@@ -4,7 +4,7 @@
 
 
 
-## Read measures from files .txt of a 1 instance MTS
+## Read measures from files .txt of a one instance BTS
 read_measures <- function(dir, class_name, tmeasure_name, layer_A, layer_B = 0) {
   if(layer_B == 0)
     file_name <- paste0(dir, "/", class_name, "__", layer_A, "_", tmeasure_name, ".txt")
@@ -27,12 +27,13 @@ read_measures <- function(dir, class_name, tmeasure_name, layer_A, layer_B = 0) 
 }
 
 
-## Read measures from files .txt of a rep instances MTS
-read_measures_set <- function(dir, class_name, tmeasure_name, n, layer_A, layer_B = 0) {
+## Read measures from files .txt of BTS set
+read_measure_set <- function(dir, class_name, measure_name, i, layer_A, layer_B = 0) {
   if(layer_B == 0)
-    file_name <- paste0(dir, "/", class_name, "_", n, "__", layer_A, "_", tmeasure_name, ".txt")
+    file_name <- paste0(layer_A, "_", measure_name, ".txt")
   else
-    file_name <- paste0(dir, "/", class_name, "_", n, "__", layer_A, "-", layer_B,  "_", tmeasure_name, ".txt")
+    file_name <- paste0(layer_A, "-", layer_B,  "_", measure_name, ".txt")
+  file_name <- paste0(dir, "/", class_name, "_", i, "__", file_name)
 
   data <- read.table(file_name, header = F, sep = ",")
   data <- t(data)
