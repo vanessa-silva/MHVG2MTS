a88a18a047e89525655912765c20dda64d5a612f
vanessa-silva <vanessaaf_silva@hotmail.com>
2022-09-16 14:57:32 +0100

    adding need libraries

---
+++ libraries.R
@@ -0,0 +1,28 @@
+#####################################################
+##### load needed libraries
+#####################################################
+
+## utils 
+library(dplyr)
+library(reshape2)
+
+## time series
+library(forecast)
+
+## tables
+library(kableExtra)
+library(formattable)
+
+## plots
+library(ggplot2)
+library(corrplot)
+
+## PCA
+library(factoextra)
+
+## clustering
+library(cluster)
+
+## evaluation measures
+library(aricode)
+
